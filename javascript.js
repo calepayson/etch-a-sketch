d89ae8615e24bfb7686daf673cc7bb046e541d67
Cale Smith <calepayson@mac.com>
2024-10-03 12:46:24 -0700

    Add function to create a grid
    createGrid creates a div of grids based upon the provided height and
    width. It appends this grid to the 'container' class node of the DOM.

--- javascript.js
+++ javascript.js
@@ -1,1 +1,20 @@
+const height = 16;
+const width = 16;
 
+let createGrid = (height, width) => {
+    const container = document.querySelector(".container");
+
+    for (let i = 0; i < height; i++) {
+        const row = document.createElement('div');
+        row.classList.add('row');
+
+        for (let j = 0; j < width; j++) {
+            const cell = document.createElement('div');
+            cell.classList.add('cell');
+            row.appendChild(cell);
+        }
+        container.appendChild(row);
+    }
+}
+
+createGrid(height, width);
